cf9f15a8781c8037d61bf8ecbb95fb058e38747d
leodorna <leo.dorneles@hotmail.com>
2018-05-02 10:24:09 -0300

    Quarto Commit

---
+++ interface/Car.php
@@ -0,0 +1,24 @@
+
+<?php
+
+require_once("Vehicle.php");
+
+class Car extends Vehicle
+{
+  private $doors;
+  //private $doors;
+  //public, consegue acessar em outras partes dpo projeto;
+  //private, não pode ser acessado em outras partes do projeto;
+
+  public function setDoors($doors){
+    $this->doors = $doors;
+  }
+  public function getDoors(){
+    return $this->doors;
+  }
+  public function getBrand(){
+      return $this->brand;
+  }
+
+}
+?>
